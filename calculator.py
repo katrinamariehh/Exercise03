b8101a513aafaa2f90140cd2c9e3608036f616d2
Hackbright Student <user@mini-003.(none)>
2014-02-07 15:33:46 -0800

    Functions defined by comman[0]

--- calculator.py
+++ calculator.py
@@ -8,13 +8,24 @@ user_input = raw_input("> ")
 
 command = user_input.split()
 
-methods = {
-    "+": add,
-    "-": subtract,
-    "*": multiply
-}
+# need to deal with if command has more than two items
+# need to deal with if command other than comman[0] includes invalid input
 
 if command[0] == "+":
     print add(int(command[1]), int(command[2]))
-if command[0] == "-":
+elif command[0] == "-":
     print subtract(int(command[1]), int(command[2]))
+elif command[0] == "*":
+    print multiply(int(command[1]), int(command[2]))
+elif command[0] == "/":
+    print divide(int(command[1]), int(command[2]))
+elif command[0] == "square":
+    print square(int(command[1]))
+elif command[0] == "cube":
+    print cube(int(command[1]))
+elif command[0] == "pow":
+    print power(int(command[1]), int(command[2]))
+elif command[0] == "mod":
+    print mod(int(command[1]), int(command[2]))
+else:
+    print "That is not a valid command"
