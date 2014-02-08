956eabcec877cd35144239c1d7a5513208097c54
Hackbright Student <user@mini-003.(none)>
2014-02-07 16:53:57 -0800

    All functions working with error messages; need to change approach to checking status of inputs

--- calculator.py
+++ calculator.py
@@ -10,8 +10,9 @@ while True:
     command = user_input.split()
 
 # need to deal with if command has more than two items
-# need to deal with if command other than comman[0] includes invalid input
+# need to deal with if command other than command[0] includes invalid input
 
+     
     if command[0] == "+":
         total = 0
         for i in range(1, len(command)):
@@ -28,15 +29,30 @@ while True:
             total = multiply(int(total), int(command[i]))
         print total
     elif command[0] == "/":
-        print divide(int(command[1]), int(command[2]))
+        total = command[1]
+        for i in range(2, len(command)):
+            total = divide(int(total), int(command[i]))
+        print total
     elif command[0] == "square":
-        print square(int(command[1]))
+        if len(command) > 2:
+            print "The square function takes only one argument; please try again."
+        else:
+            print square(int(command[1]))
     elif command[0] == "cube":
-        print cube(int(command[1]))
+        if len(command) > 2:
+            print "The cube function takes only one argument; please try again."
+        else:
+            print cube(int(command[1]))
     elif command[0] == "pow":
-        print power(int(command[1]), int(command[2]))
+        total = command[1]
+        for i in range(2, len(command)):
+            total = power(int(total), int(command[i]))
+        print total
     elif command[0] == "mod":
-        print mod(int(command[1]), int(command[2]))
+        if len(command) > 3:
+            print "The modulo function takes only two arguments; please try again."
+        else:
+            print mod(int(command[1]), int(command[2]))
     elif command[0] == 'q':
         break
     else:
